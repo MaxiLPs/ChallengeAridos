74909ae424d913029c8ec6b16fc6a309cff196e3
Maximiliano Agüero <aguero.maximilianojavier@gmail.com>
2021-09-29 00:34:23 -0300

    Subiendo parte 1 y 2

---
+++ index.js
@@ -0,0 +1,160 @@
+const express = require("express");
+const app = express();
+
+var bodyParser = require("body-parser");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+var mysql = require("mysql");
+const { query } = require("express");
+const { json } = require("body-parser");
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "34421819m",
+  database: "aridos",
+}); 
+
+con.connect();
+
+app.get("/", function (req, res) {
+  res.send("Saludos desde express");
+});
+
+//ALTA DE USUARIO
+// Solo se recibe alias y pass. Luego se agrega la fecha en formato yyyy/mm/dd hh:mm:ss
+
+app.post("/usuarios", function (req, res, next) {
+  const alias = req.body["alias"];
+  const password = req.body["password"];
+
+  const ahora = new Date();
+
+  const ano = ahora.getFullYear().toString();
+  const mes = (ahora.getMonth() + 1) > 9? (ahora.getMonth() + 1).toString() : "0" + (ahora.getMonth() + 1).toString();
+  const dia = (ahora.getDate() + 1) > 9? (ahora.getDate() + 1).toString() : "0" + (ahora.getDate() + 1).toString();
+  
+  const hora = (ahora.getHours() + 1) > 9? (ahora.getHours() + 1).toString() : "0" + (ahora.getHours() + 1).toString();
+  const minutos = (ahora.getMinutes() + 1) > 9? (ahora.getMinutes() + 1).toString() : "0" + (ahora.getMinutes() + 1).toString();
+  const segundos = (ahora.getSeconds() + 1) > 9? (ahora.getSeconds() + 1).toString() : "0" + (ahora.getSeconds() + 1).toString();
+
+  const fecha = ano + "/" + mes + "/" + dia + " " + hora + ":" + minutos + ":" + segundos;
+
+
+  const sql_query= "INSERT INTO `user_base` (`alias`,`password`,`date_creation`,`state`) VALUES (?,?,?,?);";
+  const valores = [alias, password, fecha, 1];
+  
+  let query = mysql.format(sql_query, valores);
+
+function pepito (err, result, fields){
+  if (err){
+    if(err.code == 'ER_DUP_ENTRY' || err.errno == 1062)
+    {
+      res.status(409).send({
+        status: 409,
+        messenge: "El usuario ya existe",
+      });
+    }
+    else{
+      res.status(500).send({
+        status: 500,
+        messenge: "Error de servidor",
+      });
+    }
+  } else {
+    res.status(201).send({
+      status: 201,
+      messenge: "Se agregó al usuario ID: " + result.insertId ,
+    });
+  }
+}
+
+
+result= con.query("SELECT * FROM user_base")
+
+  con.query(query, function(err, result, fields){
+    if (err){
+      if(err.code == 'ER_DUP_ENTRY' || err.errno == 1062)
+      {
+        res.status(409).send({
+          status: 409,
+          messenge: "El usuario ya existe",
+        });
+      }
+      else{
+        res.status(500).send({
+          status: 500,
+          messenge: "Error de servidor",
+        });
+      }
+    } else {
+      res.status(201).send({
+        status: 201,
+        messenge: "Se agregó al usuario ID: " + result.insertId ,
+      });
+    }
+  });
+});
+
+//BUSCA Y DEVUELVE TODOS LOS USUARIOR 
+app.get("/usuarios", function (req, res) {
+  // const alias = req.query.alias;
+  const query = "SELECT * FROM `user_base`";
+
+    con.query(query, function (err, result, fields) {
+      if (err) throw err;
+      res.send(JSON.stringify(result));
+    });
+});
+
+//BUSCA Y UN USUARIO SEGÚN ALIAS
+
+app.get("/usuarios/:alias", function (req, res) {
+  const alias = req.params.alias;
+  const sql_query= "SELECT * FROM `user_base` WHERE alias = ? ;";
+  const valores = [alias];
+  
+  const query = mysql.format(sql_query, valores);
+
+  con.query(query, function (err, result, fields) {
+    if (err) throw err;
+    const consulta = JSON.stringify(result[0]);
+
+    if (consulta){
+      res.status(200).send({
+        usuario: JSON.parse(consulta),
+      });
+    }else{
+      res.status(400).send({
+        status: 400,
+        messenge: "Alias no válido"
+      });
+    }
+  });
+});
+
+app.get("/grupos", function (req, res) {
+  const alias = req.query.alias;
+  const sql_query= "SELECT alias as 'nombre', nombre as 'lugar' FROM user_security us, user_base ub, security_groups sg " + 
+  "WHERE us.user_base_id = ub.idusuario AND us.security_groups_id = sg.id AND alias =?";
+  const valores = [alias];
+
+  const query = mysql.format(sql_query, valores);
+
+  con.query(query, function (err, result, fields) {
+    if (err) throw err;
+    res.send(JSON.stringify(result));
+  });
+
+});
+
+
+app.listen(3000, () => {
+  console.log("El servidor está inicializado en el puerto 3000");
+});
